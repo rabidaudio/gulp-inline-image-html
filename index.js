6c331d093ac96cb122ee8b8a267666ab92647200
Charles Julian Knight <charles@rabidaudio.com>
2015-03-07 01:06:35 -0500

    working with buffers

--- index.js
+++ index.js
@@ -3,29 +3,34 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 
-var PLUGIN_NAME = 'gulp-inline-image';
+const PLUGIN_NAME = 'gulp-inline-image';
 
-function inlineImage() {
+function inlineImage(dir) {
 
   var stream = through.obj(function(file, enc, cb) {
-    if (file.isBuffer()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
+
+    if(file.isNull()){
+      return cb(null, file);
+    }
+
+    if (file.isStream()) {
+      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
       return cb();
     }
 
-    console.log(file);
+    // must be a buffer
+    file.contents = inlineimg(file.contents, dir);
 
-    if (file.isStream()) {
+      // var streamer = through(function(chunk, enc, callback){
+      //   this.push(inlineimg(chunk));
+      //   callback();
+      // });
 
-      var streamer = through(function(chunk, enc, callback){
-        this.push(inlineimg(chunk));
-        callback();
-      });
-
-      streamer.on('error', this.emit.bind(this, 'error'));
-      file.contents = file.contents.pipe(streamer);
-    }
-
+      // streamer.on('error', this.emit.bind(this, 'error'));
+      // file.contents = file.contents.pipe(streamer);
+    //       if (file.isStream()) {
+    //   file.contents = file.contents.pipe(prefixStream(prefixText));
+    // }
     this.push(file);
     cb();
   });
