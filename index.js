ba9d8e966276d5ff069a3a821b4b8a9ff2109aac
Charles Julian Knight <charles@rabidaudio.com>
2015-03-07 01:53:52 -0500

    commented out stream test, refactored I don't think it is easily possible to allow streams

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var inlineimg = require('inline-images');
-var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 
@@ -8,31 +7,16 @@ const PLUGIN_NAME = 'gulp-inline-image';
 function inlineImage(dir) {
 
   var stream = through.obj(function(file, enc, cb) {
-
-    if(file.isNull()){
-      return cb(null, file);
-    }
-
     if (file.isStream()) {
       this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
       return cb();
     }
-
-    // must be a buffer
-    file.contents = inlineimg(file.contents, dir);
-
-      // var streamer = through(function(chunk, enc, callback){
-      //   this.push(inlineimg(chunk));
-      //   callback();
-      // });
-
-      // streamer.on('error', this.emit.bind(this, 'error'));
-      // file.contents = file.contents.pipe(streamer);
-    //       if (file.isStream()) {
-    //   file.contents = file.contents.pipe(prefixStream(prefixText));
-    // }
-    this.push(file);
-    cb();
+    if (file.isBuffer()) {
+      file.contents = inlineimg(file.contents, dir);
+      this.push(file);
+      return cb();
+    }
+    return cb(null, file); //no-op
   });
 
   return stream;
