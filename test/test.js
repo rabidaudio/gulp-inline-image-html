a92bbc7acd01165469d37c08b6bb6d3885e618a1
Charles Julian Knight <charles@rabidaudio.com>
2015-03-07 01:22:25 -0500

    working buffer mode

---
+++ test/test.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+var es = require('event-stream');
+var File = require('vinyl');
+var fs = require('fs');
+
+var inlineimg = require('../');
+
+describe('gulp-inline-image', function(){
+
+  describe('in buffer mode', function(){
+
+    it('should replace images with data tags', function(done){
+      var fakeFile = new File({
+        contents: fs.readFileSync('test/fixtures/index.html')
+      });
+      var myInlineimg = inlineimg('test/fixtures');
+      myInlineimg.write(fakeFile);
+      myInlineimg.once('data', function(file) {
+
+        expect(file.isBuffer()).to.be.true;
+
+        expect(file.contents.toString('utf8')).not.to.contain('peppers.png');
+        expect(file.contents.toString('utf8')).to.contain('src="data:');
+
+        done();
+      });
+    });
+  });
+
+   // describe('in streaming mode', function() {});
+});
