4968444caccb8f318181ad1880fb2226f2447abb
Sergey Kupletsky <s.kupletsky@gmail.com>
2017-08-04 05:55:22 +0300

    Check file for existence

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inlineimg = require('inline-images');
 var gutil = require('gulp-util');
+var fs = require('fs');
 var PluginError = gutil.PluginError;
 
 const PLUGIN_NAME = 'gulp-inline-image';
@@ -7,14 +8,16 @@ const PLUGIN_NAME = 'gulp-inline-image';
 function inlineImage(dir) {
 
   var stream = through.obj(function(file, enc, cb) {
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
-      return cb();
-    }
-    if (file.isBuffer()) {
-      file.contents = inlineimg(file.contents, dir);
-      this.push(file);
-      return cb();
+    if (fs.existsSync(file)) {
+      if (file.isStream()) {
+        this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+        return cb();
+      }
+      if (file.isBuffer()) {
+        file.contents = inlineimg(file.contents, dir);
+        this.push(file);
+        return cb();
+      }
     }
     return cb(null, file); //no-op
   });
